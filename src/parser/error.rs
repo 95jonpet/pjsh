7fc4a4a8b70f16d92033a5dfb0d23e83d90f87c7
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2021-07-21 21:05:31 +0200

    Remove unused enum

--- src/parser/error.rs
+++ src/parser/error.rs
@@ -5,7 +5,6 @@ use crate::token::Token;
 #[derive(Debug, PartialEq)]
 pub enum ParseError {
     UnconsumedToken(Token),
-    UnexpectedCharSequence,
     UnexpectedToken(Token),
 }
 
@@ -13,7 +12,6 @@ impl Display for ParseError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::UnconsumedToken(token) => write!(f, "unconsumed token {}", token),
-            Self::UnexpectedCharSequence => write!(f, "unexpected character sequence"),
             Self::UnexpectedToken(token) => write!(f, "unexpected token {}", token),
         }
     }
