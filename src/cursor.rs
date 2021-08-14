93bf1994833b63f16f62ac18e04a4828cdd8c307
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2021-08-14 22:02:45 +0200

    Add tests

--- src/cursor.rs
+++ src/cursor.rs
@@ -94,3 +94,76 @@ impl Cursor {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_peeks_the_next_char() {
+        assert_eq!(cursor("12").peek(), &'1', "return the first character");
+        assert_eq!(cursor("").peek(), &EOF_CHAR, "no more input, return EOF");
+    }
+
+    #[test]
+    fn it_returns_the_next_char() {
+        let mut cursor = cursor("12");
+        assert_eq!(cursor.next(), '1', "return the first character");
+        assert_eq!(cursor.next(), '2', "return the second character");
+        assert_eq!(cursor.next(), EOF_CHAR, "no more input, return EOF");
+        assert_eq!(cursor.next(), EOF_CHAR, "don't panic on further reads");
+    }
+
+    #[test]
+    fn it_advances_lines() {
+        let mut cursor = multiline_cursor(vec!["1\n", "2\n"]);
+        assert_eq!(cursor.peek(), &'1', "first character of the first line");
+        cursor.advance_line("");
+        assert_eq!(cursor.peek(), &'2', "first character of the second line");
+    }
+
+    #[test]
+    fn it_knows_if_it_is_interactive() {
+        let options = Rc::new(RefCell::new(Options::default()));
+        let non_interactive = Cursor::new(InputLines::Single(None), false, options.clone());
+        let interactive = Cursor::new(InputLines::Single(None), true, options);
+
+        assert_eq!(non_interactive.is_interactive(), false);
+        assert_eq!(interactive.is_interactive(), true);
+    }
+
+    #[test]
+    fn it_can_skip_characters() {
+        let mut cursor = cursor("12");
+        cursor.skip('1');
+        assert_eq!(cursor.peek(), &'2');
+    }
+
+    #[test]
+    #[should_panic(expected = "expected character '2' but saw '1'")]
+    fn it_panics_when_trying_to_skip_invalid_characters() {
+        let mut cursor = cursor("12");
+        cursor.skip('2');
+    }
+
+    fn multiline_cursor(lines: Vec<&str>) -> Cursor {
+        let io_cursor = io::Cursor::new(lines.join(""));
+        let mut cursor = Cursor::new(
+            InputLines::Buffered(Box::new(io_cursor)),
+            false,
+            Rc::new(RefCell::new(Options::default())),
+        );
+        cursor.advance_line(""); // Force the first line of input to be read.
+        cursor
+    }
+
+    fn cursor(input: &str) -> Cursor {
+        let mut cursor = Cursor::new(
+            InputLines::Single(Some(String::from(input))),
+            false,
+            Rc::new(RefCell::new(Options::default())),
+        );
+        cursor.advance_line(""); // Force the first line of input to be read.
+        cursor
+    }
+}
