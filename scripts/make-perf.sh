2a60ad7b51059a089cc913a7a203f3a43c13e14e
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2022-06-29 22:06:38 +0200

    Add profiling script

---
+++ scripts/make-perf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Profile the application
+
+set -euo pipefail
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+TARGET_DIR="${SCRIPT_DIR}/../target"
+PJSH="${TARGET_DIR}/release/pjsh"
+EXAMPLE="$(realpath "${1?'Missing script to profile'}")"
+
+WORKDIR="$(mktemp -d)"
+trap 'rm -rf -- "${WORKDIR}"' EXIT
+
+if [[ ! -f "${PJSH}" ]]; then
+  (
+    cd "${SCRIPT_DIR}/../"
+    CARGO_PROFILE_RELEASE_DEBUG=true cargo build --release
+  )
+fi
+
+cd "${WORKDIR}"
+sudo perf record --call-graph dwarf "${PJSH}" "${EXAMPLE}" > /dev/null
+sudo chown "$(id -u):$(id -g)" perf.data
+
+perf script \
+  | stackcollapse-perf.pl \
+  | rustfilt \
+  | flamegraph.pl \
+  > "${TARGET_DIR}/flame.svg"
+
+cp perf.data "${TARGET_DIR}"
