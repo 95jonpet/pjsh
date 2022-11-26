88184561d8291b4e1e76dc2cdceb94057f4eae92
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2022-11-26 22:43:27 +0100

    Split contextualized and uncontextualized complete

---
+++ crates/pjsh/src/complete/contextualized_complete.rs
@@ -0,0 +1,11 @@
+use pjsh_core::Context;
+
+/// Completes a word based on a prefix.
+pub fn complete(
+    _prefix: &str,
+    _words: &[&str],
+    _word_index: usize,
+    _context: &Context,
+) -> Vec<String> {
+    vec![]
+}
