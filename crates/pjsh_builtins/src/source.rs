119bec4db0aec6521d18b9ebbe2f61461858af94
Peter Jonsson <4572231+95jonpet@users.noreply.github.com>
2022-12-29 19:46:45 +0100

    Add tests for the source builtin

--- crates/pjsh_builtins/src/source.rs
+++ crates/pjsh_builtins/src/source.rs
@@ -70,10 +70,11 @@ fn source_file(opts: SourceOpts, args: &mut Args) -> CommandResult {
     // This operation is destructive, so the original arguments must be stored temporarily in order
     // to restore the context before exiting.
     let mut args = Vec::with_capacity(opts.args.len() + 1);
-    let source_file_name = opts.file.file_name().map_or_else(
-        || String::from("pjsh"),
-        |name| name.to_string_lossy().to_string(),
-    );
+    let source_file_name = opts
+        .file
+        .file_name()
+        .map(|name| name.to_string_lossy().to_string())
+        .expect("the source file should be an existing file");
     args.push(source_file_name);
     args.extend(opts.args);
 
@@ -81,4 +82,99 @@ fn source_file(opts: SourceOpts, args: &mut Args) -> CommandResult {
     // executing a file. Thus, this must be performed using a shell action.
     let action = Action::SourceFile(opts.file, args);
     CommandResult::with_actions(status::SUCCESS, vec![action])
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::{HashMap, HashSet};
+
+    use pjsh_core::{command::BuiltinCommandResult, Context, Scope};
+    use tempfile::NamedTempFile;
+
+    use crate::utils::empty_io;
+
+    use super::*;
+
+    /// Runs tests for [`Source`] and [`SourceShorthand`].
+    fn test(context: Context, func: impl Fn(&str, &BuiltinCommandResult)) {
+        let cmd = Source;
+        let mut io = empty_io();
+        let mut ctx = context.try_clone().expect("context should be cloned");
+        let mut args = Args::new(&mut ctx, &mut io);
+        let CommandResult::Builtin(result) = cmd.run(&mut args) else {
+            unreachable!("Built-ins always return specialized built-in results");
+        };
+        func(cmd.name(), &result);
+
+        let cmd = SourceShorthand;
+        let mut io = empty_io();
+        let mut ctx = context;
+        let mut args = Args::new(&mut ctx, &mut io);
+        let CommandResult::Builtin(result) = cmd.run(&mut args) else {
+            unreachable!("Built-ins always return specialized built-in results");
+        };
+        func(cmd.name(), &result);
+    }
+
+    #[test]
+    fn it_prints_help() {
+        let ctx = Context::with_scopes(vec![Scope::new(
+            String::new(),
+            Some(vec!["source".into(), "--help".into()]),
+            HashMap::default(),
+            HashMap::default(),
+            HashSet::default(),
+        )]);
+
+        test(ctx, |name, result| {
+            assert_eq!(result.code, 0, "`{name} --help` should exit successfully");
+        });
+    }
+
+    #[test]
+    fn it_sources_files() -> std::io::Result<()> {
+        let script_file = NamedTempFile::new()?;
+
+        let ctx = Context::with_scopes(vec![Scope::new(
+            String::new(),
+            Some(vec!["source".into(), path_to_string(script_file.path())]),
+            HashMap::default(),
+            HashMap::default(),
+            HashSet::default(),
+        )]);
+
+        test(ctx, |name, result| {
+            assert_eq!(result.code, 0, "`{name} FILE` should exit successfully");
+            assert!(
+                matches!(
+                &result.actions[0],
+                Action::SourceFile(p, _) if p == script_file.path()),
+                "`{name} FILE` should return an action to source FILE"
+            );
+        });
+        Ok(())
+    }
+
+    #[test]
+    fn it_errors_on_missing_file_to_source() -> std::io::Result<()> {
+        let ctx = Context::with_scopes(vec![Scope::new(
+            String::new(),
+            Some(vec!["source".into(), "/missing/file".into()]),
+            HashMap::default(),
+            HashMap::default(),
+            HashSet::default(),
+        )]);
+
+        test(ctx, |name, result| {
+            assert_ne!(
+                result.code, 0,
+                "`{name} MISSING_FILE` should not exit successfully"
+            );
+            assert!(
+                result.actions.is_empty(),
+                "`{name} MISSING_FILE` should not return an action to source MISSING_FILE"
+            );
+        });
+        Ok(())
+    }
 }
